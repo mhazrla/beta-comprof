8680730f3a4562e41710361582c203b97717268e
mhazrla <mhazrla@gmail.com>
2022-11-22 14:04:42 +0700

    no msg

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,14 +1,25 @@
 import Navbar from "@/Components/Navbar";
 import Hero from "@/Layouts/Homepage/Hero";
 import IndiDo from "@/Layouts/Homepage/IndiDo";
+import { usePage } from "@inertiajs/inertia-react";
 import React from "react";
 
 const Homepage = () => {
+    const { asset } = usePage().props;
+    console.log(asset);
+
+    const image = {
+        logo: asset + "/image/Homepage/logo.png",
+        hero: asset + "/image/Homepage/hero.png",
+        circle: asset + "/image/Homepage/circle.png",
+        searching: asset + "/image/Homepage/searching.png",
+    };
+
     return (
         <div>
             <Navbar />
-            <Hero />
-            <IndiDo />
+            <Hero asset={image} />
+            <IndiDo asset={image} />
         </div>
     );
 };
